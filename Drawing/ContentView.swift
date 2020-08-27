68cb74fc3d01cf24ba3b9ea31e587a0c10aa93ca
Matthew Richardson <mat_richardson@mac.com>
2020-08-27 14:36:40 +1000

    Image Border

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,50 +8,15 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    // How much to move this petal away from the center
-    var petalOffset: Double = -20
-    
-    // How wide to make each petal
-    var petalWidth: Double = 100
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
-            
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
-        }
-        
-        return path
-    }
-}
 
 struct ContentView: View {
     
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    
     
     var body: some View {
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(Color.red, style: FillStyle(eoFill: true))
-            
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .vertical])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-            
-        }
+        Text("Hello World")
+        .frame(width: 300, height: 300)
+        .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
     }
 }
 
